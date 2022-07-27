2460c272b8c6e4fa4b317d26981f7fe38874da51
99Page <nou0ggid@gmail.com>
2022-07-27 14:38:48 +0900

    title err fix

--- Ren2U/Ren2UTab.swift
+++ Ren2U/Ren2UTab.swift
@@ -18,6 +18,7 @@ struct Ren2UTab: View {
     
     @EnvironmentObject var groupModel: GroupModel
     @State private var tabSelection: TabSelection?
+    @State private var searchText = ""
     @State private var title = ""
     @State private var isShowingTitle = true
     
@@ -39,16 +40,16 @@ struct Ren2UTab: View {
     
     var body: some View {
         NavigationView {
-            TabView(selection: $tabSelection) {
+            TabView() {
                 Home()
                     .tabItem {
                         Image(systemName: "house")
                         Text("홈")
                     }
-                    .tag(TabSelection.home)
                     .onAppear {
                         self.title = "Home"
-                        self.isShowingTitle = false
+                        self.isShowingTitle = true
+                        self.tabSelection = .home
                     }
 
                 GroupMain()
@@ -56,10 +57,11 @@ struct Ren2UTab: View {
                         Image(systemName: "person.2")
                         Text("그룹")
                     }
-                    .tag(TabSelection.group)
+                    .navigationBarTitleDisplayMode(.inline)
                     .onAppear {
                         self.title = "Group"
-                        self.isShowingTitle = false
+                        self.isShowingTitle = true
+                        self.tabSelection = .group
                     }
                 
                 
@@ -68,33 +70,72 @@ struct Ren2UTab: View {
                         Image(systemName: "rectangle.on.rectangle")
                         Text("대여")
                     }
-                    .tag(TabSelection.rent)
                     .onAppear {
                         self.title = "Rent"
-                        self.isShowingTitle = false
+                        self.isShowingTitle = true
+                        self.tabSelection = .rent
                     }
-                
+
                 MyPage()
                     .tabItem {
                         Image(systemName: "person")
                         Text("마이페이지")
                     }
-                    .tag(TabSelection.myPage)
                     .onAppear {
                         self.title = "MyPage"
-                        self.isShowingTitle = false
+                        self.isShowingTitle = true
+                        self.tabSelection = .myPage
                     }
             }
             .navigationTitle(title)
             .navigationBarHidden(!isShowingTitle)
+            .navigationBarTitleDisplayMode(.inline)
             .accentColor(.Navy_1E2F97)
             .foregroundColor(.LabelColor)
+            .toolbar(content: {
+                ToolbarItemGroup(placement: .principal) {
+                    switch tabSelection {
+                    case .home, .group, .none:
+                        SearchBar()
+                    case .rent, .myPage:
+                        EmptyView()
+                    }
+                }
+            })
             .onAppear {
                 groupModel.fetchNotices()
                 groupModel.fetchRentalItems()
             }
         }
     }
+    
+    @ViewBuilder
+    func SearchBar() -> some View {
+        
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.Gray_DEE2E6)
+                TextField("I`ll Ren2U", text: $searchText)
+            }
+            .padding(5)
+            .overlay {
+                Capsule()
+                    .stroke(Color.Gray_DEE2E6, lineWidth: 2)
+            }
+            
+            Image(systemName: "bell")
+        }
+    }
+    
+    @ViewBuilder
+    func EmptyView() -> some View {
+        
+        HStack {
+            Text("툴바 예정")
+        }
+    }
+   
 }
 
 struct Ren2UTab_Previews: PreviewProvider {
